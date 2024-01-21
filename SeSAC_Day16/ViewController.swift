a9d997c4ea4be14cf6133f59ca9483ae178dcdcc
jeonghi <jpark0902@kookmin.ac.kr>
2024-01-21 14:02:37 +0900

    Initial Commit

---
+++ SeSAC_Day16/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SeSAC_Day16
+//
+//  Created by 쩡화니 on 1/21/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
